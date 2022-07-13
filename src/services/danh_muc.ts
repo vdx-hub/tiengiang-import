9ed3c0c9bdfa47235723979b3b3e9de6faf86bc2
Nguyễn Trung Hiếu <nth4forum@gmail.com>
2022-07-13 15:42:48 +0700

    feat: import v2

--- src/services/danh_muc.ts
+++ src/services/danh_muc.ts
@@ -2,23 +2,21 @@ import { _client } from "@db/mongodb";
 import { log } from "console";
 import fs from "fs-extra"
 interface ImportConfig {
-  "Name": string,
   "DanhMuc": string,
   "KeySearch": string,
-  "Fields": [string]
+  "Fields": string[]
 }
 
-async function getDanhMuc(db: string, config: ImportConfig, renewDanhMuc: string) {
+async function getDanhMuc(db: string, config: ImportConfig, cacheDanhMuc: string) {
   let danhMuc: any = {};
   try {
     danhMuc = await fs.readJSON(`tmp/${db}___${config.DanhMuc}.json`)
   }
   catch (err) {
-    log(err)
   }
-  if (Object.entries(danhMuc).length > 0 && renewDanhMuc == 'false') return danhMuc;
+  if (Object.entries(danhMuc).length > 0 && cacheDanhMuc == 'true') return danhMuc;
   // query
-  if (renewDanhMuc == 'true') {
+  if (cacheDanhMuc == 'false') {
     danhMuc = {}
     let projectFields: any = {
       [config.KeySearch]: 1
